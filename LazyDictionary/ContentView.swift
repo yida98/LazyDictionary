70d00d5400f37ac72727b53211623c3b1c43f16f
Yida Da <carriedada@gmail.com>
2021-07-22 13:41:09 -0700

    first commit

---
+++ LazyDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazyDictionary
+//
+//  Created by Yida Zhang on 2021-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
